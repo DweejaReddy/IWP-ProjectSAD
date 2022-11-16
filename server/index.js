15228563d00747a6b0f3861f5dd7abc1a5ab0ece
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-11-16 21:14:15 +0530

    student login register and mentor login done

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express')
-const app = express();
+
 const dotenv =require("dotenv");
+const app = express();
 dotenv.config({path: './config.env'});
+
+const PORT = process.env.PORT || 8000;
+require("./schema/userschema");
 require('./db/connect');
 
-const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
+app.use(require("./router/auth"));
 app.get('/', (req, res) => {
       res.send(' Ramuduu')
-      })
+ })
+
 
 app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`)
       }
-)
+);
